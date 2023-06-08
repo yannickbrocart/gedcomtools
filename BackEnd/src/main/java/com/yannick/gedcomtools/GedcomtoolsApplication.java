5ac941063f1893d718a381ec50ac44a95b99072d
yannickbrocart <mail@yannickbrocart.com>
2023-06-08 21:05:23 +0200

    maj + angular

---
+++ BackEnd/src/main/java/com/yannick/gedcomtools/GedcomtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.yannick.gedcomtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedcomtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedcomtoolsApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/yannick/gedcomtools/GedcomtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yannick.gedcomtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedcomtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
