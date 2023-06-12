8077ca3411c500aba9442939489bfd0e3dbb37fe
yannickbrocart <mail@yannickbrocart.com>
2023-06-12 16:27:42 +0200

    Spring boot work!

--- backend/src/main/java/com/yannick/gedcomtools/GedcomtoolsApplication.java
+++ backend/src/main/java/com/yannick/gedcomtools/GedcomtoolsApplication.java
@@ -2,8 +2,12 @@ package com.yannick.gedcomtools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GedcomtoolsApplication {
 
 	public static void main(String[] args) {
