13f4c30451ad0cb1a8d13b744b91104bd2a51da1
yannickbrocart <mail@yannickbrocart.com>
2023-06-16 12:25:08 +0200

    maj Controls

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,12 +10,13 @@ import { ReadFileComponent } from './read-file/read-file.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ReadFileComponent,
+    ReadFileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/read-file/read-file.component.ts
+++ frontend/src/app/read-file/read-file.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component  } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-read-file',
-  templateUrl: './read-file.component.html',
-  styleUrls: ['./read-file.component.css']
+	selector: 'app-read-file',
+	templateUrl: './read-file.component.html',
+	styleUrls: ['./read-file.component.css']
 })
+
 export class ReadFileComponent {
-	fileInput: string;
+	
+	gedPattern = "^[a-zA-Z]$";
+	fileInputControl = new FormControl(null, [Validators.required]);
 	
 	constructor() { } 
 	
-
-		    
-	submit(){}
-
+	fileImputChangeValue() {
+		console.log("CHANGE " + this.fileInputControl.value);
+	}
+  
+	submitFileInput(){
+		console.log('Name: ' + this.fileInputControl.value);
+		console.log('Validation Status: ' + this.fileInputControl.status);
+	}
 }
