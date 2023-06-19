7014d2641a175902ba7e8f15a805e5111d8f8834
yannickbrocart <mail@yannickbrocart.com>
2023-06-19 20:32:41 +0200

    maj

--- frontend/src/app/read-file/read-file.component.ts
+++ frontend/src/app/read-file/read-file.component.ts
@@ -1,5 +1,5 @@
 import { Component  } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-read-file',
@@ -9,17 +9,30 @@ import { FormControl, Validators } from '@angular/forms';
 
 export class ReadFileComponent {
 	
-	gedPattern = "^[a-zA-Z]$";
-	fileInputControl = new FormControl(null, [Validators.required]);
+	gedPattern = "^.*\\.ged$";
+	projectNamePattern = "^[a-z0-9_-]{1,60}$";
+	
+	fileForm = new FormGroup({
+	    fileFullPath: new FormControl('', Validators.required),
+	    projectName: new FormControl('', 
+	    		[Validators.required, 
+	    		 Validators.minLength(3), 
+	    		 Validators.maxLength(60),
+	    		 Validators.pattern(this.projectNamePattern)]),	    
+	});
 	
 	constructor() { } 
 	
 	fileImputChangeValue() {
-		console.log("CHANGE " + this.fileInputControl.value);
+		console.log("CHANGE " + this.fileForm.value);
 	}
   
-	submitFileInput(){
-		console.log('Name: ' + this.fileInputControl.value);
-		console.log('Validation Status: ' + this.fileInputControl.status);
+	onSubmit(){
+		console.log('Path: ' + this.fileForm.controls.fileFullPath.value);
+		console.log('Path validation status: ' + this.fileForm.controls.fileFullPath.status);
+		var filePath = this.fileForm.controls.fileFullPath.value;
+		var fileName = filePath.substr(filePath.lastIndexOf('\\') + 1);
+		console.log('Name: ' + this.fileForm.controls.projectName.value);
+		console.log('Name validation status: ' + this.fileForm.controls.projectName.status);
 	}
 }
