f85758d1d977e3a5a8e77fe14fdf2835e03f3f09
yannickbrocart <mail@yannickbrocart.com>
2023-06-20 16:38:54 +0200

    maj

--- frontend/src/app/read-file/read-file.component.ts
+++ frontend/src/app/read-file/read-file.component.ts
@@ -11,12 +11,12 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ReadFileComponent {
 	
 	gedPattern = "^.*\\.ged$";
-	projectNamePattern = "^[a-z0-9_-]{0,60}$";
+	projectNamePattern = "^[a-zA-Z]{1,}[\\w-]{2,60}$";
 	fileName!: string;
 	filePath!: string;
 	fileString!: string;
-	fileLastModified!: number;
-	fileSize!: number;
+	fileLastModified!: Date;
+	fileSize!: string;
 	fileType!: string;
 		
 	fileForm = new FormGroup({
@@ -30,14 +30,15 @@ export class ReadFileComponent {
 	
 	constructor(private http: HttpClient) {} 
 	
+	
 	readGedcomFile() {}
 	
 	fileFullPathChangeValue(fullPath : any) {
 		const file: File = fullPath.target.files[0];
 		this.filePath = this.fileForm.controls.fileFullPath.value as string;
-		if (!this.fileForm.controls.fileFullPath.errors) this.fileName = file.name;	
-		this.fileLastModified = file.lastModified;
-		this.fileSize = file.size;
+		this.fileName = file.name;	
+		this.fileLastModified = new Date(file.lastModified);
+		this.fileSize = formatBytes(file.size,2);
 		this.fileType = file.type;
 
 	}
@@ -49,3 +50,11 @@ export class ReadFileComponent {
 	onSubmit(){
 	}
 }
+
+function formatBytes(bytes: number, dm = 2) {
+	  if (bytes == 0) return '0 Bytes';
+	  var k = 1024,
+	    sizes = [`${bytes <= 1 ? "Byte" : "Bytes"}`, 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'],
+	    i = Math.floor(Math.log(bytes) / Math.log(k));
+	  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+	}
