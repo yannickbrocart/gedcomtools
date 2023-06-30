66500b365bbd4be63a91468993268709ae7fa4d6
yannickbrocart <mail@yannickbrocart.com>
2023-06-30 19:56:06 +0200

    maj

--- backend/src/main/java/com/yannick/gedcomtools/repository/UserRepository.java
+++ backend/src/main/java/com/yannick/gedcomtools/repository/UserRepository.java
@@ -8,11 +8,5 @@ import com.yannick.gedcomtools.model.Gedcom;
 import com.yannick.gedcomtools.model.GedcomStatusType;
 import com.yannick.gedcomtools.model.User;
 
-public interface GedcomRepository extends JpaRepository<Gedcom, Long> {
-	
-	List<Gedcom> findByStatus(GedcomStatusType status);
-
-	List<Gedcom> findByCreated(User createdBy);
-
-	List<Gedcom> findByLastModified(User lastModifiedBy);
+public interface UserRepository extends JpaRepository<User, Long> {
 }
